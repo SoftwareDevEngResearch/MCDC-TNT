2add0471ad943268c2aa843097fd91d0b8ad179d
Jackson Porter Morgan <jack@pop-os.localdomain>
2022-02-18 12:39:17 -0800

    added source particles test

--- tests/unit/tests_unit.py
+++ tests/unit/tests_unit.py
@@ -1,4 +1,4 @@
-import mcdc-tnt
+import mcdc_tnt.pp_kernels as kernels
 
 #class test_pp_kernels:
     #def Advance(self):
@@ -12,10 +12,38 @@ import mcdc-tnt
     
     #def FissionsAdd(self)
     
+def test_SourceParticles():
+    num_parts = 5
+    p_pos_x = np.zeros(num_parts)
+    p_pos_y = np.zeros(num_parts)
+    p_pos_z = np.zeros(num_parts)
     
+    p_mesh_cell = np.zeros(num_parts)
+    
+    p_dir_x = np.zeros(num_parts)
+    p_dir_y = np.zeros(num_parts)
+    p_dir_z = np.zeros(num_parts)
+    
+    p_speed = np.zeros(num_parts)
+    p_time = np.ones(num_parts)
+    p_alive = np.zeros(num_parts)
+    
+    particle_speed = 1
+    meshwise_fission_pdf = [0,1]
+    
+    iso=False
+    
+    dx = 0.2
+    
+    [p_pos_x, p_pos_y, p_pos_z, p_mesh_cell, p_dir_y, p_dir_z, p_dir_x, p_speed, p_time, p_alive] = SourceParticles(p_pos_x, p_pos_y, p_pos_z, p_mesh_cell, dx, p_dir_y, p_dir_z, p_dir_x, p_speed, p_time, p_alive, num_parts, meshwise_fission_pdf, particle_speed, iso)
+    
+    assert (np.sum(p_time) == 0)
+    assert (p_mesh_cell.all() == 1)
+    assert (p_alive.all() == True)
+    assert (p_pos_x.all() > .2)
 
-def test_SampleEvent(self):
-    p_mesh_cell = np.array[0,1,0,5]
+def test_SampleEvent():
+    p_mesh_cell = np.array([0,1,0,5])
     p_alive = [True,True,True,False]
     
     mesh_cap_xsec = 1/3*np.ones(2)
@@ -26,17 +54,22 @@ def test_SampleEvent(self):
     capture_event_index = np.zeros(3)
     fission_event_index = np.zeros(3) 
     
-    rands = np.array([.2, .4, .8])
+    controled_rands = np.array([.2, .4, .8])
     
     nu = 2
     num_part = 3
     
-    [scatter_event_index, scat_count, capture_event_index, cap_count, fission_event_index, fis_count] = SampleEvent(p_mesh_cell, p_event, p_alive, mesh_cap_xsec, mesh_scat_xsec, mesh_fis_xsec, scatter_event_index, capture_event_index, fission_event_index, num_part, no, rands)
+    [scatter_event_index, scat_count, capture_event_index, cap_count, fission_event_index, fis_count] = kernels.SampleEvent(p_mesh_cell, p_alive, mesh_cap_xsec, mesh_scat_xsec, mesh_fis_xsec, scatter_event_index, capture_event_index, fission_event_index, num_part, nu, controled_rands)
     
     assert (fis_count == 1)
     assert (scat_count == 1)
     assert (cap_count == 1)
     
     assert (capture_event_index[0] == 1)
-    assert (fission_event_index[1] == 1)
-    assert (scatter_event_index[2] == 1)
+    assert (fission_event_index[0] == 2)
+    assert (scatter_event_index[0] == 0)
+        
+if __name__ == '__main__':
+    test_SampleEvent()
+    
+    
